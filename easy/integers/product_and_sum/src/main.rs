2962b1d2c008cb8247e6ccff714369de3c00b09e
Vlad <accierro@gmail.com>
2020-04-08 21:56:00 +0100

    Add implementation of product_and_sum

---
+++ easy/integers/product_and_sum/src/main.rs
@@ -0,0 +1,43 @@
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+// Example 1:
+
+// Input: n = 234
+// Output: 15
+// Explanation:
+// Product of digits = 2 * 3 * 4 = 24
+// Sum of digits = 2 + 3 + 4 = 9
+// Result = 24 - 9 = 15
+// Example 2:
+
+// Input: n = 4421
+// Output: 21
+// Explanation:
+// Product of digits = 4 * 4 * 2 * 1 = 32
+// Sum of digits = 4 + 4 + 2 + 1 = 11
+// Result = 32 - 11 = 21
+
+// Constraints:
+
+// 1 <= n <= 10^5
+
+struct Solution;
+
+impl Solution {
+    pub fn subtract_product_and_sum(mut n: i32) -> i32 {
+        let mut product = 1;
+        let mut sum = 0;
+        while n > 0 {
+            let digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+        product - sum
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::subtract_product_and_sum(234), 15);
+    assert_eq!(Solution::subtract_product_and_sum(4421), 21);
+}
