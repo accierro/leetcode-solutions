03f16c8c9180651635488384f7ef862658bf5bc6
Vlad <accierro@gmail.com>
2020-07-06 12:23:43 +0100

    [medium][dp] fix

--- medium/dp/counting_bits/src/main.rs
+++ medium/dp/counting_bits/src/main.rs
@@ -4,14 +4,8 @@ impl Solution {
     pub fn count_bits(num: i32) -> Vec<i32> {
         let mut result = vec![0];
 
-        let mut current_max = 0;
         for i in 1..num + 1 {
-            if result[(i as usize) - 1] > current_max {
-                current_max += 1;
-                result.push(1);
-            } else {
-                result.push(result[(i as usize) - 1] + 1);
-            }
+            result.push(result[i as usize >> 1] + (i & 1));
         }
 
         result
@@ -20,5 +14,6 @@ impl Solution {
 
 fn main() {
     assert_eq!(Solution::count_bits(0), vec![0]);
+    assert_eq!(Solution::count_bits(4), vec![0, 1, 1, 2, 1]);
     assert_eq!(Solution::count_bits(5), vec![0, 1, 1, 2, 1, 2]);
 }
