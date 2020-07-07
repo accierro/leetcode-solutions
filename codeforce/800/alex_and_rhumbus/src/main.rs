caa561f2c02ce3bbe506cb34962bdec586fc9a67
Vlad <accierro@gmail.com>
2020-07-07 19:22:14 +0100

    [codeforce] alex and thumbs

---
+++ codeforce/800/alex_and_rhumbus/src/main.rs
@@ -0,0 +1,33 @@
+use std::cmp::{max, min};
+use std::io::{stdin, stdout, BufWriter, Write};
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>,
+}
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+
+fn main() {
+    let mut scanner = Scanner::default();
+
+    let n = scanner.next::<usize>();
+
+    let mut dp = vec![1];
+
+    for i in 1..n {
+        dp.push(dp[i - 1] + i * 4);
+    }
+
+    println!("{}", dp[n - 1]);
+}
